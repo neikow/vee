465b245b2635a9eb4f8c3d619477b1a43cc753d8
vitaly <50383391+Neikow@users.noreply.github.com>
2025-11-21 20:45:23 +0100

    fix offset error

--- main.cpp
+++ main.cpp
@@ -161,7 +161,7 @@ struct Vertex {
 
         attributeDescriptions[2].binding = 0;
         attributeDescriptions[2].location = 2;
-        attributeDescriptions[2].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
         attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
 
 
